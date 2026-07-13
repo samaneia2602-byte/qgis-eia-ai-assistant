e886cb0fa7902343b8b067d3ba1ec09643ddbd8b
samaneia2602-byte <GitHub  >
2026-07-13 14:14:02 +0900

    feat: add NGII API key setting

--- settings.py
+++ settings.py
@@ -1,21 +1,45 @@
 # -*- coding: utf-8 -*-
+
 from qgis.core import QgsSettings
 
-ORG = 'qgis_eia_ai_assistant'
+
+ORG = "qgis_eia_ai_assistant"
+
 
 class SettingsStore:
     def __init__(self):
         self.s = QgsSettings()
 
-    def get(self, key, default=''):
-        return self.s.value('%s/%s' % (ORG, key), default)
+    def get(self, key, default=""):
+        return self.s.value(
+            "%s/%s" % (ORG, key),
+            default,
+        )
 
     def set(self, key, value):
-        self.s.setValue('%s/%s' % (ORG, key), value or '')
+        self.s.setValue(
+            "%s/%s" % (ORG, key),
+            value or "",
+        )
 
     @property
-    def vworld_key(self): return self.get('vworld_key')
+    def vworld_key(self):
+        return self.get("vworld_key")
+
     @property
-    def eia_key(self): return self.get('eia_key')
+    def eia_key(self):
+        return self.get("eia_key")
+
     @property
-    def ecology_key(self): return self.get('ecology_key')
+    def ecology_key(self):
+        return self.get("ecology_key")
+
+    @property
+    def ngii_key(self):
+        """
+        국토지리정보원 국토정보플랫폼 OpenAPI 인증키.
+
+        QGIS 사용자 설정에 저장되며 플러그인 소스나 ZIP에는
+        직접 기록되지 않습니다.
+        """
+        return self.get("ngii_key")
--- ui/api_settings.py
+++ ui/api_settings.py
@@ -1,29 +1,133 @@
 # -*- coding: utf-8 -*-
-from qgis.PyQt.QtWidgets import QDialog, QVBoxLayout, QFormLayout, QLineEdit, QDialogButtonBox, QLabel
+
+from qgis.PyQt.QtWidgets import (
+    QCheckBox,
+    QDialog,
+    QDialogButtonBox,
+    QFormLayout,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+)
+
 from ..settings import SettingsStore
+
 
 class ApiSettingsDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle('API Key 설정')
+
+        self.setWindowTitle("API Key 설정")
+        self.resize(620, 300)
+
         self.store = SettingsStore()
+
         layout = QVBoxLayout(self)
-        layout.addWidget(QLabel('API Key는 QGIS 사용자 설정에 저장되며 플러그인 ZIP에는 저장되지 않습니다.'))
+        guide = QLabel(
+            "API Key는 QGIS 사용자 설정에 저장되며 "
+            "플러그인 ZIP이나 GitHub에는 저장되지 않습니다."
+        )
+        guide.setWordWrap(True)
+        layout.addWidget(guide)
+
         form = QFormLayout()
-        self.vworld = QLineEdit(self.store.vworld_key)
-        self.eia = QLineEdit(self.store.eia_key)
-        self.ecology = QLineEdit(self.store.ecology_key)
-        form.addRow('VWorld API Key', self.vworld)
-        form.addRow('환경영향평가 API serviceKey', self.eia)
-        form.addRow('생태자연도 API serviceKey', self.ecology)
+
+        self.vworld = self._key_edit(
+            self.store.vworld_key
+        )
+        self.eia = self._key_edit(
+            self.store.eia_key
+        )
+        self.ecology = self._key_edit(
+            self.store.ecology_key
+        )
+        self.ngii = self._key_edit(
+            self.store.ngii_key
+        )
+
+        self.ngii.setPlaceholderText(
+            "국토정보플랫폼에서 발급된 OpenAPI 인증키"
+        )
+
+        form.addRow(
+            "VWorld API Key",
+            self.vworld,
+        )
+        form.addRow(
+            "환경영향평가 API serviceKey",
+            self.eia,
+        )
+        form.addRow(
+            "생태자연도 API serviceKey",
+            self.ecology,
+        )
+        form.addRow(
+            "국토지리정보원 API Key",
+            self.ngii,
+        )
+
         layout.addLayout(form)
-        buttons = QDialogButtonBox(QDialogButtonBox.Save | QDialogButtonBox.Cancel)
+
+        self.show_keys = QCheckBox(
+            "입력한 API Key 표시"
+        )
+        self.show_keys.toggled.connect(
+            self._toggle_key_visibility
+        )
+        layout.addWidget(self.show_keys)
+
+        buttons = QDialogButtonBox(
+            QDialogButtonBox.Save
+            | QDialogButtonBox.Cancel
+        )
+        buttons.button(
+            QDialogButtonBox.Save
+        ).setText("저장")
+        buttons.button(
+            QDialogButtonBox.Cancel
+        ).setText("취소")
         buttons.accepted.connect(self.save)
         buttons.rejected.connect(self.reject)
         layout.addWidget(buttons)
 
+    def _key_edit(self, value):
+        edit = QLineEdit(value or "")
+        edit.setEchoMode(
+            QLineEdit.Password
+        )
+        edit.setClearButtonEnabled(True)
+        return edit
+
+    def _toggle_key_visibility(self, checked):
+        mode = (
+            QLineEdit.Normal
+            if checked
+            else QLineEdit.Password
+        )
+
+        for edit in (
+            self.vworld,
+            self.eia,
+            self.ecology,
+            self.ngii,
+        ):
+            edit.setEchoMode(mode)
+
     def save(self):
-        self.store.set('vworld_key', self.vworld.text().strip())
-        self.store.set('eia_key', self.eia.text().strip())
-        self.store.set('ecology_key', self.ecology.text().strip())
+        self.store.set(
+            "vworld_key",
+            self.vworld.text().strip(),
+        )
+        self.store.set(
+            "eia_key",
+            self.eia.text().strip(),
+        )
+        self.store.set(
+            "ecology_key",
+            self.ecology.text().strip(),
+        )
+        self.store.set(
+            "ngii_key",
+            self.ngii.text().strip(),
+        )
         self.accept()
